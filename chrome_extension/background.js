13d4d548106b969c8123b6679654f46f39e5a345
Salsasp <dakotagee99@gmail.com>
2023-10-16 12:28:22 -0500

    redoing lost progress

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,6 +1,7 @@
 console.log("background.js loaded");
 var sessionBegin, sessionEnd, sessionTotal;
 var sessionComplete = false, switchState = false;
+var sessionHistoryItems, sessionVisitItems = [];
 
 chrome.runtime.onMessage.addListener(function(message){
     if(message.switchEnabled){
@@ -15,14 +16,74 @@ chrome.runtime.onMessage.addListener(function(message){
         sessionComplete = true;
         console.log(false)
         console.log("session complete");
+        chrome.history.search(
+            {
+                text: '',                   // Return every history item
+                startTime: sessionBegin,    // that was accessed in between sessionBegin   
+                endTime: sessionEnd         // and sessionEnd
+            },
+            function(historyItems) {
+                // For each history item, get details on all visits
+                sessionHistoryItems = historyItems;
+                for (var i = 0; i < historyItems.length; ++i) {
+                    var url = historyItems[i].url;
+                    console.log(url);
+                    generateNodes(historyItems);
+                }
+            }
+        );
+        class urlNode {
+            constructor(url, visitTime, title, previousUrls) {
+                this.url = url;
+                this.visitTime = visitTime;
+                this.title = title;
+                this.previousUrl = previousUrls;
+            }
+        }
+
+
+        const generateNodes = function (historyItems) {
+            console.log("generateNodes called")
+            let urlNodes = new Array();
+            let visitToHistoryMap = new Map();
+            historyItems.forEach(item => {
+                console.log("in history items loop")
+                chrome.history.getVisits({url: item.url}), function(visits){     
+                    console.log("visits size: " + visits.length)  
+                    visits.forEach(visit => {
+                        console.log("in visits loop")
+                        visitToHistoryMap.set(visit, item); //for quick access between historyItem properties and visit properties
+                        sessionVisitItems.push(visit); //pushes all visits into sessionVisitItems
+                        let tempNode = new urlNode(item.url, visit.visitTime, item.title, getPreviousUrl(visit));
+                        urlNodes.push(tempNode);
+                    });
+                }
+            });
+            urlNodes.forEach(element => { //debugging
+                console.log(element.title);
+            });
+        };
+
+        function getPreviousUrl(visit) {
+            console.log("getPreviousUrl called")
+            console.log("transition: " + visit.transition)
+            console.log("referringVisitId: " + visit.referringVisitId)
+            if(visit.transition != link || visit.referringVisitId == null){
+                return [];
+            }
+            correspondingHistoryItem = visitToHistoryMap.get(visit);
+            let tempVisit;
+            sessionVisitItems.forEach(visitItem => { //loop to find matching visit of referringVisitId
+                if(visitItem.id == visit.referringVisitId){
+                    tempVisit = visitItem;
+                }
+            });
+            if(tempVisit == null){
+                return [];
+            }
+            let tempHistoryItem = visitToHistoryMap.get(tempVisit);
+            console.log("MATCH! - " + tempHistoryItem.url)
+            return tempHistoryItem.url;
+        }
     }
-});
-
-chrome.webNavigation.onCommitted.addListener(function(data){
-    if (data.frameId !== 0 || sessionComplete) return;
-
-    var currentUrl;
-    var tabIdToUrl = {};
-    tabIdToUrl[data.tabId.toString()] = data.url;
-    chrome.storage.local.set(tabIdToUrl);
 });
