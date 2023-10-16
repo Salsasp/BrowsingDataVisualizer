cbd38383311a124d05adcaf68a5a66e24ba28e89
Salsasp <dakotagee99@gmail.com>
2023-10-16 00:21:04 -0500

    using chrome.webNavigation

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -15,68 +15,14 @@ chrome.runtime.onMessage.addListener(function(message){
         sessionComplete = true;
         console.log(false)
         console.log("session complete");
-        chrome.history.search(
-            {
-                text: '',                   // Return every history item
-                startTime: sessionBegin,    // that was accessed in between sessionBegin   
-                endTime: sessionEnd         // and sessionEnd
-            },
-            function(historyItems) {
-                // For each history item, get details on all visits.
-                for (var i = 0; i < historyItems.length; ++i) {
-                    var url = historyItems[i].url;
-                    console.log(url);
-                    var processVisitsWithUrl = function(url) {
-                        // We need the url of the visited item to process the visit.
-                        // Use a closure to bind the  url into the callback's args.
-                        return function(visitItems) {
-                            generateNodes(visitItems);
-                        };
-                    };
-                    chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
-                }
-            }
-        );
-        class urlNode {
-            constructor(url, visitTime) {
-                this.url = url;
-                this.visitTime = visitTime;
-                this.previousUrl = null;
-                this.nextUrls = new Array();
-            }
-            setPreviousUrl(url) {
-                this.previousUrl = url;
-            }
-            appendNextUrl(url) {
-                this.nextUrls.append(url);
-            }
-            setVisitTime(time) {
-                this.visitTime = time;
-            }
-        }
-
-
-        const generateNodes = function (visitItems) {
-            let urlNodes = new Array();
-            for (let i = 0; i < visitItems.length; ++i) {
-                if (visitItems[i].transition != 'link') {
-                    let newNode = new urlNode(visitItems[i].url, visitItems[i].title, visitItems[i].visitTime); 
-                    console.log(visitItems[i].url, visitItems[i].title, visitItems[i].visitTime);
-                    urlNodes.push(newNode);
-                }
-                else{
-                    let newNode = new urlNode(visitItems[i].url, visitItems[i].title, visitItems[i].visitTime); 
-                    
-                    let prevVisitId = visitItems[i].referringVisitId;
-                }  
-            }
-
-            // If this is the final outstanding call to processVisits(),
-            // then we have the final results.  Use them to build the list
-            // of URLs to show in the popup.
-            urlNodes.forEach(element => {
-                console.log(element.title);
-            });
-        };
     }
+});
+
+chrome.webNavigation.onCommitted.addListener(function(data){
+    if (data.frameId !== 0 || sessionComplete) return;
+
+    var currentUrl;
+    var tabIdToUrl = {};
+    tabIdToUrl[data.tabId.toString()] = data.url;
+    chrome.storage.local.set(tabIdToUrl);
 });
