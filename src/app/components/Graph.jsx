50de2aa6246b433801f16adfaf7dd046dfb90ba9
Noah Zinn <noahwzinn@gmail.com>
2023-11-01 17:18:49 -0500

    Added bounds checking to graph, and node staying under mouse

--- src/app/components/Graph.jsx
+++ src/app/components/Graph.jsx
@@ -3,9 +3,8 @@ import { useState } from 'react'
 
 
 // SVG element for a node
-function Node({x, y, setActive}) {
+function Node({x, y, radius = 10, setActive}) {
 
-  const radius = 10;
   const fill = '#66FF19';
   
   return (
@@ -24,18 +23,31 @@ function Link({source, target}) {
 }
 
 
+function getNumLinks(nodeName, links) {
+
+  let count = 0;
+
+  // Count how nodes are connected to nodeName
+  for (const link of links) {
+    if (link.source == nodeName || link.target == nodeName) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
 
 export default function Graph({nodes, links,  width = 400, height = 500}) {
 
-  const [graphNodes, setGraphNodes] = useState(nodes);
 
   // Node that is currently being dragged
   const [activeNode, setActiveNode] = useState(null);
-
+  const [graphNodes, setGraphNodes] = useState(nodes);
   
   // Color for the background of the graph
   const backgroundFill = '#eeeeee'
-
+  const baseNodeSize = 10;
 
   // Function called when mouse moves over the graph
   const mouseMoved = e => {
@@ -43,15 +55,19 @@ export default function Graph({nodes, links,  width = 400, height = 500}) {
     
     // If the mouse is held down on a node, update its coordinates 
     if(activeNode) {
-
-
       
       // Create a copy to edit the coords
       const nodesCopy = {...nodes};
 
-      // Move the dragged node
-      nodesCopy[activeNode].x = nodesCopy[activeNode].x + e.movementX;
-      nodesCopy[activeNode].y = nodesCopy[activeNode].y + e.movementY;
+
+      // New location of node
+      const deltaX = nodesCopy[activeNode].x + e.movementX;
+      const deltaY = nodesCopy[activeNode].y + e.movementY;
+
+
+      // Move the dragged node, if it hits the bound keep it where it is
+      nodesCopy[activeNode].x = deltaX > width || deltaX < 0 ? nodesCopy[activeNode].x : deltaX;
+      nodesCopy[activeNode].y = deltaY > height || deltaY < 0 ? nodesCopy[activeNode].y : deltaY;
 
       
       // Update nodes
@@ -59,6 +75,14 @@ export default function Graph({nodes, links,  width = 400, height = 500}) {
     }
   }
 
+  const mouseIn = e => {
+    const nodesCopy = {...nodes};
+
+    if (activeNode) {
+      nodesCopy[activeNode].x = e.pageX - (e.target.offsetLeft || 0);
+      nodesCopy[activeNode].y = e.pageY - (e.target.offsetTop || 0);
+    }
+  }
 
   // When the mouse isn't being held down, set the active node to null
   const mouseUp = () => {
@@ -69,16 +93,16 @@ export default function Graph({nodes, links,  width = 400, height = 500}) {
   
   return (
     <div id="Graph">
-      <svg viewBox={'0 0 ' + width + ' ' + height} width={width} height={height} onMouseMove={mouseMoved} onMouseUp={mouseUp}>
+      <svg viewBox={'0 0 ' + width + ' ' + height} width={width} height={height} onMouseMove={mouseMoved} onMouseUp={mouseUp} onMouseEnter={mouseIn}>
 	
 	<rect width={width} height={height} fill={backgroundFill} />
 	
 	{links && links.map((link, i) => <Link key={i}
-					       source={nodes[link.source]}
-					       target={nodes[link.target]} />)}
+					       source={graphNodes[link.source]}
+					       target={graphNodes[link.target]} />)}
 
 	
-	{nodes && Object.entries(nodes).map(([nodeName, node]) => <Node key={nodeName}
+	{nodes && Object.entries(graphNodes).map(([nodeName, node]) => <Node key={nodeName}
 									x={node.x}
 									y={node.y}
 									url={node.url}
