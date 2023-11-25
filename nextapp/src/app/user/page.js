15befa3b0d4a6bd9f1b442ae34899e8974f06118
Mark Castle <marklouiscastle@gmail.com>
2023-11-24 19:49:06 -0600

    finished login, register, and newData(for the browser extention) and set up DB connection

--- nextapp/src/app/user/page.js
+++ nextapp/src/app/user/page.js
@@ -1,3 +1,32 @@
+//USER PAGE
+"use client";
+import { useState, useEffect } from 'react';
+
 export default function Page() {
-    return <h1>User</h1>
+  const [user, setUser] = useState(null);
+  const [browsingData, setBrowsingData] = useState(null);
+
+  useEffect(() => {
+    function getCookies() {
+      const cookieString = document.cookie;
+      const cookieArray = cookieString.split('; ');
+      const cookieObject = {};
+
+      cookieArray.forEach(cookie => {
+        const [name, value] = cookie.split('=');
+        cookieObject[name] = decodeURIComponent(value);
+      });
+
+      return cookieObject;
+    }
+
+    const cookies = getCookies();
+    const user = cookies.user ? JSON.parse(cookies.user) : null;
+    const browsingData = cookies.browsingData ? JSON.parse(cookies.browsingData) : null;
+
+    setUser(user);
+    setBrowsingData(browsingData);
+  }, []);
+
+  return <h1>User: {user}, Browsing Data: {JSON.stringify(browsingData)}</h1>;
 }
